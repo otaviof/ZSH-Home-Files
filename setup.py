511c2aaf27ac1da682850a37b332339d1abf8425
Otávio Fernandes <otaviof@gmail.com>
2010-07-29 13:59:19 +0200

    Better approach when writing Python.

--- setup.py
+++ setup.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python
 
-"""
-Very simple script for help in ZSH's files versioning.
-"""
+"""Simple script for helping ZSH's files versioning."""
 
 import os
 import difflib
@@ -11,19 +9,23 @@ import shutil
 import sys
 
 def main():
-    """Copying new, newer or unexistent files"""
+    """Copying new, newer or inexistent local files"""
     zfiles = glob.glob('%s/.z*' % os.getenv('HOME'))
     for z in zfiles:
         if not os.path.isfile(z):
             continue
         name = os.path.basename(z).lstrip('.')
-        if  not os.path.isfile(name) or (difflib.SequenceMatcher(None, open(z).read(), open(name).read()) and (os.path.getmtime(z) > os.path.getmtime(name))):
+        is_different = difflib.SequenceMatcher(
+                None, open(z).read(), open(name).read())
+        is_newer = os.path.getmtime(z) > os.path.getmtime(name)
+        if not os.path.isfile(name) or (is_different and is_newer):
             try:
                 print "Copying: %s, %s" % (z, name)
                 shutil.copyfile(z, name)
             except OSError, err:
-                print err
-                sys.exit(2)
+                print err and sys.exit(2)
 
 if __name__ == '__main__':
     main()
+
+# EOF
